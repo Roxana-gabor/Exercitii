c59bce479c0f6301b51fc1cd491bb2970803e40a
Roxana Gabor <Roxana Gabor>
2016-11-06 22:42:57 +0200

    Exercitiu 1 basic

---
+++ Exercitii/ExercitiileBasic.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercitii
+{
+    class ExercitiileBasic
+    {
+        // exercitiul 1 C# Basic Declaration
+        public void exercitiu1() {
+            System.Console.WriteLine("  Exercitiul 1: Afiseaza Hello si numele tau pe 2 linii diferite.");
+            System.Console.WriteLine("   Hello");
+            System.Console.WriteLine("   Gabor Roxana-Mihaela");
+            System.Console.WriteLine("--------------------------------");
+        }
+
+
+       
+    }
+}
---
+++ Exercitii/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercitii
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExercitiileBasic ex1 = new ExercitiileBasic();
+            ex1.exercitiu1();
+           
+
+
+            Console.ReadKey();
+        }
+    }
+
+}
